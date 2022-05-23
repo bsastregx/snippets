c2b0b3a7c90e58c881594ceffc9e2b4258b954fa
Bruno Sastre <bsastre@genexus.com>
2022-05-23 19:28:53 -0300

    update

--- features-table-v2/scripts.js
+++ features-table-v2/scripts.js
@@ -2,10 +2,10 @@
 
 comparePlans = document.querySelector(".compare-plans");
 comparePlans.addEventListener('click', function(){
-    document.querySelector('.compare-plans').scrollIntoView({
+    document.querySelector('#features-tables-top').scrollIntoView({
         behavior: 'smooth'
     });
-});
+})
 
 //ANIMATE STICKY PLANS HEADER ====================================================
 
@@ -23,17 +23,17 @@ if (entry.boundingClientRect.top <= 0) {
 }, {
 root: null,
 threshold: 0,
-});
+})
 observer.observe(pdct)
 
 //UP ARROW (GO UP TO STICKY PLANS HEADER) ====================================================
 
 const goUp = document.querySelector(".go-top"); 
 goUp.addEventListener('click', function(){
-    document.querySelector('.compare-plans').scrollIntoView({
+    document.querySelector('#features-tables-top').scrollIntoView({
         behavior: 'smooth'
     });
-});
+})
 
 // TOGGLE PLAN FEATURES (MOBILE) ====================================================
 
@@ -44,7 +44,7 @@ togglePlanFeatures.forEach(tpf => {
         const tableContainer = card.querySelector('.plan-card__table-container');
         tableContainer.classList.toggle('collapsed');
         tpf.classList.toggle('collapsed');
-    });
+    })
 });
 
 // HIDE PLAN FEATURES (MOBILE) ====================================================
@@ -64,8 +64,92 @@ const moreInfoTds = document.querySelectorAll('.more-info__td');
 moreInfoTds.forEach(moreInfoTd => {
     moreInfoTd.addEventListener('click', function(){
         moreInfoTd.classList.toggle('more-info__td--hidden');
-    });
+    })
 });
+
+// EXTRA COST ====================================================
+const extraCostBtn = document.querySelector('.extra-cost__btn');
+const planDinamicPrice = document.querySelector('.plan--dinamic-price');
+const planDinamicPricePrice = planDinamicPrice.querySelector('.plan__price__value');
+const planDinamicPricePriceSticky = planDinamicPrice.querySelector('.plan__price--sticky');
+extraCostBtn.addEventListener('click', function(){
+    if(extraCostBtn.classList.contains('extra-cost__btn--add')) {
+        planDinamicPricePrice.innerHTML = "$4.650";
+        planDinamicPricePriceSticky.innerHTML = "$4.650";
+    } else {
+        planDinamicPricePrice.innerHTML = "$4.400";
+        planDinamicPricePriceSticky.innerHTML = "$4.400";
+    }
+    planDinamicPrice.classList.add('plan--toggleBackgroundColor');
+    setTimeout(() => {
+        planDinamicPrice.classList.remove('plan--toggleBackgroundColor');
+    }, 250)
+    planDinamicPrice.classList.toggle('plan--show-extra-cost');
+    extraCostBtn.classList.toggle('extra-cost__btn--add');
+});
+
+// EXTRA COST V2 ====================================================
+// const extraCostTags = document.querySelectorAll('.tag-extra-cost[data-extra]');
+// extraCostTags.forEach(extraCostTag => {
+    
+
+//     //CREATE ELEMENTS
+//     //div .extra-cost
+//     const divExtraCost = document.createElement('div');
+//     divExtraCost.classList.add('extra-cost');
+
+//     //div .extra-cost__value-cp
+//     const divExtraCostValueCp = document.createElement('div');
+//     divExtraCostValueCp.classList.add('extra-cost__value-cp');
+
+//     //span .extra-cost__value
+//     const spanExtraCostValue = document.createElement('span');
+//     spanExtraCostValue.classList.add('extra-cost__value');
+//     spanExtraCostValue.innerHTML = extraCostTag.dataset.extra;
+
+//     //span .extra-cost__currency-perodicity
+//     const spanExtraCurrencyPeriodicity = document.createElement('span');
+//     spanExtraCurrencyPeriodicity.classList.add('extra-cost__currency-perodicity');
+//     spanExtraCurrencyPeriodicity.innerHTML = 'USD/mo';
+
+//     //button .extra-cost__btn
+//     const btnExtraCost = document.createElement('button');
+//     btnExtraCost.classList.add('extra-cost__btn','extra-cost__btn--add');
+
+//     //span .extra-cost__btn__add
+//     const spanExtraCostBtnAdd = document.createElement('span');
+//     spanExtraCostBtnAdd.classList.add('extra-cost__btn__add');
+//     spanExtraCostBtnAdd.innerHTML = 'Add';
+
+//     //span .extra-cost__btn__remove
+//     const spanExtraCostBtnRemove = document.createElement('span');
+//     spanExtraCostBtnRemove.classList.add('extra-cost__btn__remove');
+//     spanExtraCostBtnRemove.innerHTML = 'Remove';
+
+//     //div .extra-cost__triangle
+//     const divExtraCostTriangle = document.createElement('div');
+//     divExtraCostTriangle.classList.add('extra-cost__triangle');
+
+//     //ADD FUNCTIONALITY
+//     btnExtraCost.addEventListener('click', function(){
+//         if(this.classList.contains('extra-cost__btn--add')) {
+            
+//         } else {
+//             console.log('restar');
+//         }
+//         this.classList.toggle('extra-cost__btn--add');
+//     })
+
+//     //appends
+//     divExtraCostValueCp.appendChild(spanExtraCostValue);
+//     divExtraCostValueCp.appendChild(spanExtraCurrencyPeriodicity);
+//     btnExtraCost.appendChild(spanExtraCostBtnAdd);
+//     btnExtraCost.appendChild(spanExtraCostBtnRemove);
+//     divExtraCost.appendChild(divExtraCostValueCp);
+//     divExtraCost.appendChild(btnExtraCost);
+//     divExtraCost.appendChild(divExtraCostTriangle);
+//     extraCostTag.appendChild(divExtraCost);
+// });
 
 // GENERATE OTHER PLANS (MOBILE) ====================================================
 /*The following code copies for each plan, the others plans features*/
@@ -171,13 +255,12 @@ currentPlans.forEach( (currentPlan, i) => {
 
         nextOtherPlan.classList.toggle('show');
         nextNextOtherPlan.classList.toggle('show');
-        
         tips.forEach(tip => {
             tip.classList.add('tip--hidden');
             setTimeout(function(){
                 tip.classList.add('tip--height0');
             },250);
             
-        });
-    });
+        })
+    })
 });
